2d1b40d1fdac0e2630c945c597794e566f2cacbc
Nigel Jose <nigelmjose@gmail.com>
2024-09-16 14:01:53 +0100

    Remove applescript error in spotify plugin

--- .config/sketchybar/plugins/spotify.sh
+++ .config/sketchybar/plugins/spotify.sh
@@ -3,18 +3,19 @@
 update ()
 {
   PLAYING=$(osascript -e 'tell application "Spotify" to player state as string')
-  TRACK=$(osascript -e 'tell application "Spotify" to name of current track as string')
-  ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track as string')
-  ALBUM=$(osascript -e 'tell application "Spotify" to album of current track as string')
 
   if [ $PLAYING = "playing" ]; then
+    TRACK=$(osascript -e 'tell application "Spotify" to name of current track as string')
+    ARTIST=$(osascript -e 'tell application "Spotify" to artist of current track as string')
+    ALBUM=$(osascript -e 'tell application "Spotify" to album of current track as string')
+
     if [ "$ARTIST" == "" ]; then
       LABEL="$TRACK - $ALBUM"
     else
       LABEL="$TRACK - $ARTIST"
     fi
   else
-    MEDIA=""
+    LABEL=""
   fi
 
   sketchybar --animate tanh 30 --set $NAME label="$LABEL"
